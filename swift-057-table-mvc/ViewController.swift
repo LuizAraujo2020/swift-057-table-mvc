10f565d624e0ede3d128b621d3d8968b2b6ea612
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-11-02 09:40:34 -0300

    Initial Commit

---
+++ swift-057-table-mvc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-057-table-mvc
+//
+//  Created by Luiz Araujo on 02/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
